18b74e1f75c07968904817120101c77c240e4f3b
Thitikorn Nupan <thitikorn.n@g-able.com>
2025-03-27 21:40:04 +0700

    add feature
    - add section learing about dynamic form group and binding dynamic attributes tage

--- src/app/components/learning-form-groups/learning-form-groups.component.ts
+++ src/app/components/learning-form-groups/learning-form-groups.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-learning-form-groups',
@@ -8,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class LearningFormGroupsComponent {
 
+  protected usernameField() {
+    return {
+      fieldName: 'username',
+      fieldData : new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(20)])
+    };
+  }
+
+  protected passwordField() {
+    return {
+      fieldName: 'password',
+      fieldData : new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(20)])
+    };
+  }
+
+  protected rememberField() {
+    return {
+      fieldName: 'remember',
+      fieldData : new FormControl(false, [Validators.required])
+    };
+  }
 }
